e781f110ec0439dbdff2f3d17c8ba02a3444fe19
higorworkout <higor.15nwa@gmail.com>
2025-12-26 16:45:14 -0300

    changed test

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -15,7 +15,7 @@ describe('MainContent', () => {
     render(<MainContent />);
 
     const button = screen.getByRole('button');
-    await userEvent.click(button);
+    await userEvent.click(button)
     expect(screen.getByTestId('help-area')).not.toBeInTheDocument();
   });
 });
